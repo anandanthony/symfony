b12c0785ee74e69bc3e6aa819e323802f07e9ffb
anandanthony <anfranci@microsoft.com>
2020-09-10 13:21:30 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-echo "PROD-1-CXP";
+echo "PROD-1a-CXP";
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
